7cbc29ae8d200737464f7e9676ada79c308e50d4
Larissa <larissa@fantasticocomic.com>
2021-08-24 14:26:11 -0500

    Add log data

---
+++ src/Mucks/logs.js
@@ -0,0 +1,14 @@
+export default {
+  log_Ins_Outs: [
+    {
+      uuid:'1a980c94-a0d9-46c3-b47e-481dbff10a2c',
+      created_at: '2022-02-30 10:36:35.519071+00',
+      quantity: '80',
+      user_id: 'larissa',
+      sku: 'D11090317',
+      type: 'entrada',
+      from_to: 'Del Valle',
+      num_order: 'ABCD-1501'
+    }
+  ]
+}
