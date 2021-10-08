028928390993fe182dc9eab54791d6ff741f2b7b
Larissa <larissa@fantasticocomic.com>
2021-10-08 13:11:09 -0500

    Add service for entries details

--- src/api/entries.api.js
+++ src/api/entries.api.js
@@ -2,9 +2,11 @@ import { MAIN_SERVICE } from "../services/index";
 
 const GET_COUNTING = "/v1/warehouse/counts/";
 const POST_PRELOAD = "/v1/warehouse/preload";
+const GET_PRELOAD_DETAIL= "/v1/warehouse/counts/"
 
 const getCountingOrder = (idCount) => MAIN_SERVICE.get(GET_COUNTING + idCount);
 const addPreload = ({ countId, type, fromTo, assignedTo }) =>
   MAIN_SERVICE.post(POST_PRELOAD, { countId, type, fromTo, assignedTo });
+const getOrderDetail = (idCounting) => MAIN_SERVICE.get(GET_PRELOAD_DETAIL+idCounting+'/detail?limit=10&page=1')
 
-export { getCountingOrder, addPreload };
+export { getCountingOrder, addPreload, getOrderDetail };
