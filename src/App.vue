695aeb256115cbeeaa4becda8c58e9c28f9e5bec
Larissa <larissa@fantasticocomic.com>
2021-10-11 13:22:59 -0500

    Refactor sidebar items

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
       <div class="container__main">
         <div :class="isOpen ? 'container__in' : 'container__in-full'">
           <Sidebar @btnClick="expandMenu" :modifier="{ opened: isOpen }">
-            <MenuItems></MenuItems>
+            <MenuItems :items="sidebarMenu" />
           </Sidebar>
           <router-view />
         </div>
@@ -21,6 +21,7 @@
 import MenuItems from "@/components/MenuItems";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
+import sidebarItems from "./Mucks/sidebar";
 
 export default {
   name: "app",
@@ -34,6 +35,7 @@ export default {
   data() {
     return {
       isOpen: false,
+      sidebarMenu: sidebarItems.items,
     };
   },
 
--- src/components/MenuItems.vue
+++ src/components/MenuItems.vue
@@ -41,78 +41,18 @@
 export default {
   name: "FMenuItems",
 
+  props:{
+    items: {
+      type: Array
+    }
+  },
+
   data() {
     return {
       openedItems: {},
       selectedSub: "",
       userMenu: false,
-      items: [
-        {
-          icon: "./src/assets/img/icons/dashboard.svg",
-          text: "Dashboard",
-          path: "/",
-          name: "dashboard",
-        },
-        {
-          icon: "./src/assets/img/icons/orders.svg",
-          text: "Banner",
-          path: "/banner",
-          name: "banner",
-        },
-        {
-          icon: "./src/assets/img/icons/products.svg",
-          text: "Bolsa de trabajo",
-          path: "#3",
-          name: "jobs",
-          children: [
-            {
-              icon: "now-ui-icons files_paper",
-              text: "Nested 1 ",
-              path: "/products",
-            },
-            {
-              icon: "now-ui-icons files_paper",
-              text: "Nested 2",
-              path: "/categories",
-            },
-            {
-              icon: "now-ui-icons location_bookmark",
-              text: "Nested 3",
-              path: "/attribute-sets",
-            },
-          ],
-        },
-        {
-          icon: "./src/assets/img/icons/clients.svg",
-          text: "Sucursales",
-          path: "#3",
-          name: "branches",
-        },
-        {
-          icon: "./src/assets/img/icons/marketing.svg",
-          text: "Contacto",
-          path: "#4",
-          name: "contact",
-        },
-        {
-          icon: "./src/assets/img/icons/reports.svg",
-          text: "Pedidos anticipados",
-          path: "#5",
-          name: "early",
-        },
-        {
-          icon: "./src/assets/img/icons/settings.svg",
-          text: "Pedidos entre sucursales",
-          path: "#6",
-          name: "between",
-        },
-        {
-          icon: "./src/assets/img/icons/integrations.svg",
-          text: "Preguntas frecuentes",
-          path: "#7",
-          name: "faqs",
-        },
-      ],
+      
     };
   },
 
