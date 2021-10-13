2609685beeddcaa3f5a85e81d360f53a49795388
Larissa <larissa@fantasticocomic.com>
2021-10-13 16:51:40 -0500

    Refactor layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,13 +3,6 @@
     <div class="fHeader__top">
       <div class="fHeader__top-right">
         <strong>Hola, Admin</strong>
-        <!-- <Burger>
-          <ul class="fHeader__mobile-items">
-            <li class="fHeader__mobile-item">
-              <a href="#" class="user">Hola Lara</a>
-            </li>
-          </ul>
-        </Burger> -->
       </div>
     </div>
   </header>
