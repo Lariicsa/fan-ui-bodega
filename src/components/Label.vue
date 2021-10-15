676f6bc43ce08ae65e3f45e9a162b7bbc36ee4b7
Larissa <larissa@fantasticocomic.com>
2021-10-15 16:15:44 -0500

    Add label component and UI testing view

---
+++ src/components/Label.vue
@@ -0,0 +1,9 @@
+<template>
+  <div :class="'fLabel__' + type"></div>
+</template>
+<script>
+export default {
+  name: "fLabel",
+  props: ["text", "type"],
+};
+</script>
---
+++ src/views/Testing.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container__box">
+    <div class="row center">
+      <h1>UI Testing</h1>
+    </div>
+    <div class="col">
+      <Label type="assigned" />
+    </div>
+  </div>
+</template>
+<script>
+import Label from "@/components/Label";
+export default {
+  name: "Testing",
+
+  components: {
+    Label,
+  },
+};
+</script>
